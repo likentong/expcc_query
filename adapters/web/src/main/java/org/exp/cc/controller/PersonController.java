f630dcc6274569094563296cb66c3b1e46228128
LiKen <LiKen.Tong@experian.com>
2019-08-24 16:47:56 +0800

    Update variable name.

--- adapters/web/src/main/java/org/exp/cc/controller/PersonController.java
+++ adapters/web/src/main/java/org/exp/cc/controller/PersonController.java
@@ -33,8 +33,8 @@ public class PersonController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping(path = "/queue", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ExceptionLogHandler
-    public void sendToQueue(@RequestBody Map<String, Object> data) {
-        this.personService.sendToPersonQueue(data);
+    public void sendToQueue(@RequestBody Map<String, Object> person) {
+        this.personService.sendToPersonQueue(person);
     }
 
 }
